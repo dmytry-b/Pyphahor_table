1200cb92be99970ae1b4f175f31a453bf78ad88a
dmytry-b <d20052005@yandex.ru>
2022-06-08 09:58:37 +0300

    2

---
+++ main.py
@@ -0,0 +1,24 @@
+from openpyxl import Workbook
+import os
+
+wb = Workbook()
+dest_filename = 'test_pix.xlsx'
+ws = wb.active
+ws.title = 'Piphagor'
+sise = 10  # Table size
+
+piphagor = [[0 for j in range(sise)] for i in range(sise)]
+
+
+for i in range(sise):
+    for j in range(sise):
+        piphagor[i][j] = (i + 1) * (j + 1)
+
+
+for i in range(sise):
+    for j in range(sise):
+        ws.cell(i + 1, j + 1).value = piphagor[i][j]
+
+
+wb.save(os.path.join(os.getcwd(), dest_filename))
+wb.close()
